f01907b290eb87394b058236fbca2e01d344f193
swahpy <hitwhy@gmail.com>
2026-01-21 21:19:24 +0800

    feat(keymaps): add gd as lsp go to definition keymap, too.

--- plugin/20_keymaps.lua
+++ plugin/20_keymaps.lua
@@ -209,6 +209,7 @@ nmap_leader('lh', '<Cmd>lua vim.lsp.buf.hover()<CR>',           'Hover')
 nmap_leader('lr', '<Cmd>lua vim.lsp.buf.rename()<CR>',          'Rename')
 nmap_leader('lR', '<Cmd>lua vim.lsp.buf.references()<CR>',      'References')
 nmap_leader('ls', '<Cmd>lua vim.lsp.buf.definition()<CR>',      'Source definition')
+nmap('gd',        '<Cmd>lua vim.lsp.buf.definition()<CR>',      'Source definition')
 nmap_leader('lt', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', 'Type definition')
 
 xmap_leader('lf', '<Cmd>lua require("conform").format()<CR>',   'Format selection')
