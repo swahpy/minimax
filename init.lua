a2328d7e075e6ff2fd23004849f2dfa6e7121d2c
swahpy <hitwhy@gmail.com>
2026-07-08 14:19:58 +0800

    fix: move Config.on_packchanged definition before first vim.pack.add

--- init.lua
+++ init.lua
@@ -65,6 +65,39 @@
 -- It is a global variable which can be use both as `_G.Config` and `Config`
 _G.Config = {}
 
+-- Define custom autocommand group and helper to create an autocommand.
+-- Autocommands are Neovim's way to define actions that are executed on events
+-- (like creating a buffer, setting an option, etc.).
+--
+-- See also:
+-- - `:h autocommand`
+-- - `:h nvim_create_augroup()`
+-- - `:h nvim_create_autocmd()`
+local gr = vim.api.nvim_create_augroup("custom-config", {})
+Config.new_autocmd = function(event, pattern, callback, desc)
+  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+  vim.api.nvim_create_autocmd(event, opts)
+end
+
+-- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
+-- argument to the callback. See `:h vim.pack-events`.
+-- Example usage: see 'plugin/40_plugins.lua'.
+-- If any plugin requires installation hooks, add them after this function
+-- and before the first `vim.pack.add()` call.
+Config.on_packchanged = function(plugin_name, kinds, callback, desc)
+  local f = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
+      return
+    end
+    if not ev.data.active then
+      vim.cmd.packadd(plugin_name)
+    end
+    callback(ev.data)
+  end
+  Config.new_autocmd("PackChanged", "*", f, desc)
+end
+
 -- 'mini.nvim' - all-in-one plugin powering most MiniMax features.
 -- See 'plugin/30_mini.lua' for how it is used.
 -- Load now to have 'mini.misc' available for custom loading helpers.
@@ -99,35 +132,4 @@ Config.on_event = function(ev, f)
 end
 Config.on_filetype = function(ft, f)
   misc.safely("filetype:" .. ft, f)
-end
-
--- Define custom autocommand group and helper to create an autocommand.
--- Autocommands are Neovim's way to define actions that are executed on events
--- (like creating a buffer, setting an option, etc.).
---
--- See also:
--- - `:h autocommand`
--- - `:h nvim_create_augroup()`
--- - `:h nvim_create_autocmd()`
-local gr = vim.api.nvim_create_augroup("custom-config", {})
-Config.new_autocmd = function(event, pattern, callback, desc)
-  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
-  vim.api.nvim_create_autocmd(event, opts)
-end
-
--- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
--- argument to the callback. See `:h vim.pack-events`.
--- Example usage: see 'plugin/40_plugins.lua'.
-Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-  local f = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
-      return
-    end
-    if not ev.data.active then
-      vim.cmd.packadd(plugin_name)
-    end
-    callback(ev.data)
-  end
-  Config.new_autocmd("PackChanged", "*", f, desc)
 end
